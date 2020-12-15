f7a496780c49c3790ce0637f5a065ac1d62bd452
James <j.a.pratt@outlook.com>
2020-12-15 20:58:33 +0000

    all tests passed

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-function calculateBill
+const calculateBill = (price,vat,tip) => {
 
-
-
-
-
-
-
-
-
-
-
-
+    if (typeof( price, vat, tip) === 'number') {
+ 
+return "£" + (price + (price * vat)  /100 + tip );
+} 
+else {
+   return "error"
+   }
+}
+calculateBill()
 module.exports = calculateBill;
